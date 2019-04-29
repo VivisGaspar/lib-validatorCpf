a8b3ac10593f5c835a2737e18965b18ecc054788
Viviane <viviane.a.gaspar@gmail.com>
2019-04-29 17:10:25 -0300

    adiciona funções para validação de cpf e adiciona testes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 #lib-validatorCpf
 
-##Sobre a biblioteca.
+##Sobre a biblioteca
 
 Esta biblioteca tem o objetivo de implementar um validador de CPF para uso em aplicações web. Na **versão 1.0.0** possibilita verificar os números de CPF fazendo a validação dos dígitos retornando **true** ou **false** de acordo com cada caso.
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,34 @@
 const chai = require("chai");
 const expect = chai.expect;
+const validator = require("../lib/index.js");
 
-const Number = require("../lib/index.js");
-
-describe(" validator lib", () => {
-  describe("number", () => {
-    it("A soma de 1+1 = 2", () => {
-      expect(Number.number(1, 1)).to.equal(2);
-    });
-    it("A soma de 1+2 = 3", () => {
-      expect(Number.number(2, 2)).to.equal(4);
-    });
+describe("validator", () => {
+  it(" O CPF é válido", () => {
+    expect(validator.cpfValidator("11144477735")).to.equal(true);
+  });
+  it(" O CPF é válido", () => {
+    expect(validator.cpfValidator("37355518828")).to.equal(true);
+  });
+  it(" O CPF é válido", () => {
+    expect(validator.cpfValidator("00000000000")).to.equal(false);
+  });
+  it(" O CPF é válido", () => {
+    expect(validator.cpfValidator("99999999999")).to.equal(false);
+  });
+  it(" O CPF é válido", () => {
+    expect(validator.cpfValidator("vxvxvxvxvxv")).to.equal(false);
+  });
+  it(" O CPF é válido", () => {
+    expect(validator.cpfValidator("vx@#$%¨&*()")).to.equal(false);
+  });
+  it(" O CPF é válido", () => {
+    expect(validator.cpfValidator("qvxxxxxqweq")).to.equal(false);
+  });
+  it(" O CPF é válido", () => {
+    expect(validator.cpfValidator("11111111111")).to.equal(false);
+  });
+  it(" O CPF é válido", () => {
+    expect(validator.cpfValidator("123456789")).to.equal(false);
   });
 });
+
