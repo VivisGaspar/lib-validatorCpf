7bb7234bb6fffd9bb38b83f0a1eb5e4ca3ac5ea4
Viviane <viviane.a.gaspar@gmail.com>
2019-04-30 16:12:32 -0300

    corrige descrição de testes e adiciona metodo toString na função receiveCpf

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-#lib-validatorCpf
+# lib-validatorCpf
 
-##Sobre a biblioteca
+## Sobre a biblioteca
 
 Esta biblioteca tem o objetivo de implementar um validador de CPF para uso em aplicações web. Na **versão 1.0.0** possibilita verificar os números de CPF fazendo a validação dos dígitos retornando **true** ou **false** de acordo com cada caso.
+
+## Como instalar
+
+```shell
+
+$  npm install lib-validatorcpf
+
+```
+
+## Como utilizar
+
+```node
+
+> const cpfvalidator = require("lib-validatorcpf");
+> cpfValidator.lib-validatorcpf("11144477735")
+> // returns "true"
+
+> cpfValidator.lib-validatorcpf("99999999999")
+> // returns "false"
+
+
+```
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,34 +3,40 @@ const expect = chai.expect;
 const validator = require("../lib/index.js");
 
 describe("validator", () => {
-  it(" O CPF é válido", () => {
+  it("The CPF is valid: '11144477735'", () => {
     expect(validator.cpfValidator("11144477735")).to.equal(true);
   });
-  it(" O CPF é válido", () => {
-    expect(validator.cpfValidator("37355518828")).to.equal(true);
+  it("The CPF is valid: '88601010008'", () => {
+    expect(validator.cpfValidator("88601010008")).to.equal(true);
   });
-  it("O CPF não é válido", () => {
+  it("The CPF is valid: '71650632932'", () => {
     expect(validator.cpfValidator("71650632932")).to.equal(true);
   });
-  it("O CPF não é válido", () => {
-    expect(validator.cpfValidator("00000000000")).to.equal(false);
+  it("The CPF is valid: 71650632932", () => {
+    expect(validator.cpfValidator(71650632932)).to.equal(true);
   });
-  it("O CPF não é válido", () => {
+  it("The CPF is not valid: 22222222222", () => {
+    expect(validator.cpfValidator(22222222222)).to.equal(false);
+  });
+  it("The CPF is not valid: '33333333333'", () => {
+    expect(validator.cpfValidator("33333333333")).to.equal(false);
+  });
+  it("The CPF is not valid: '99999999999'", () => {
     expect(validator.cpfValidator("99999999999")).to.equal(false);
   });
-  it("O CPF não é válido", () => {
+  it("The CPF is not valid: 'vxvxvxvxvxv'", () => {
     expect(validator.cpfValidator("vxvxvxvxvxv")).to.equal(false);
   });
-  it("O CPF não é válido", () => {
-    expect(validator.cpfValidator("vx@#$%¨&*()")).to.equal(false);
+  it("The CPF is not valid: 'vx@#$%¨&***'", () => {
+    expect(validator.cpfValidator("vx@#$%¨&***")).to.equal(false);
   });
-  it("O CPF não é válido", () => {
+  it("The CPF is not valid: 'qvxxxxxqweq'", () => {
     expect(validator.cpfValidator("qvxxxxxqweq")).to.equal(false);
   });
-  it("O CPF não é válido", () => {
+  it("The CPF is not valid: '11111111111'", () => {
     expect(validator.cpfValidator("11111111111")).to.equal(false);
   });
-  it("O CPF não é válido", () => {
+  it("The CPF is not valid: '123456789'", () => {
     expect(validator.cpfValidator("123456789")).to.equal(false);
   });
 });
